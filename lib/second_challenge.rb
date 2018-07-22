e8c9afefdfdfe343abacf9619fdc6c3b50460fb6
Felix Chan <felixchan29@gmail.com>
2018-07-22 01:28:50 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
   #code your solution here!
   answer = []
   groceries.each do |key, value|
-    for i in 0..value.length 
+    for i in 0..value.length-1 
       answer << value[i]
     end
   end
