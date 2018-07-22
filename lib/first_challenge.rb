156f105fb16b0c908ac1659712de8c5b4962e0db
Felix Chan <felixchan29@gmail.com>
2018-07-22 01:21:13 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        
+        value.each do |flavor|
     end
   end
 
